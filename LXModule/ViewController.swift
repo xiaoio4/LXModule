acd1a1fabe59ef6389a82b771d4b9aebe8f73e50
xiaoio4 <iosengineer@163.com>
2017-01-11 10:15:30 +0800

    Initial Commit

---
+++ LXModule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LXModule
+//
+//  Created by xin liu on 2017/1/11.
+//  Copyright © 2017年 xin liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
